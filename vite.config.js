f98dca81ce1cb3cf999fa628156e88388b02f95e
LorenaSTN <lorenstn@hotmail.com>
2024-12-10 15:15:05 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Buscador-personajes-Rick-y-Morty/",
+  base: "/buscador-rick-y-morty/",
 });
